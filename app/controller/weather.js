a0d9aeceba30ab28f3179dbd51b4e1360f809555
visualym <visualym@126.com>
2020-10-07 00:00:15 +0800

    add config

--- app/controller/weather.js
+++ app/controller/weather.js
@@ -6,6 +6,9 @@ class WeatherController extends Controller {
   async getWeather() {
     const { ctx } = this;
     ctx.body = 'Todo: weather';
+
+    // todo: 天气接口使用https://dev.heweather.com/docs/api/geo
+    // 可以通过城市或地区的location Id或者经纬度来查询
   }
 }
 
