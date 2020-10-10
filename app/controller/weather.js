babe1e8230575342e8402d8be173e1cb91427fb3
visualym <visualym@126.com>
2020-10-11 00:59:46 +0800

    add todo

--- app/controller/weather.js
+++ app/controller/weather.js
@@ -6,6 +6,7 @@ class WeatherController extends Controller {
   async getWeather() {
     const { ctx } = this;
     // TODO: 将的地址location作为接口参数接受和使用
+    // TODO: controller会取出固定的字段进行处理，然后将参数通过参数param传递给service
     const weatherInfo = await ctx.service.weather.get(true);
     ctx.body = weatherInfo;
 
