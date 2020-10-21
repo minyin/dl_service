abd81c67e7f3450c84d17a8d79932ec0a17c2ce9
visualym <visualym@126.com>
2020-10-21 23:37:56 +0800

    add todo

--- app/schedule/getTodayWeather.js
+++ app/schedule/getTodayWeather.js
@@ -1,6 +1,7 @@
 const Subscription = require('egg').Subscription;
 
 class GetTodayWeather extends Subscription{
+    // TODO: 建立db库表，用来存储用户的地址信息，以及每天定时读取的天气信息
     // TODO: 每天5点15分，拉取当天天气信息，然后写入db中
     // TODO: 每天7点，开始推送天气信息
     static get schedule() {
